70000fd40d27b5a27d62190946f87a148bb94e4d
Sai Kiran Anugurthi <saikiran.anugurthi@gmail.com>
2024-05-22 14:31:04 +0100

    Update variables.tf
    fix: removed creds

--- variables.tf
+++ variables.tf
@@ -2,15 +2,3 @@ variable "aws_region" {
   description = "The AWS region to create resources in."
   default     = "eu-west-1"
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
